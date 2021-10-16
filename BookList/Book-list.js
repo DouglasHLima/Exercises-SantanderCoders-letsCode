afdcbdd53c20e9e75b545a6923e24ab0ea63984c
DouglasHLima <douglashdelima@gmail.com>
2021-10-15 22:08:37 -0300

    initial project

---
+++ BookList/Book-list.js
@@ -0,0 +1,60 @@
+class Book {
+    constructor(title, genre, author, isReaded, dateReaded) {
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.isReaded = isReaded;
+        this.dateReaded = new Date(dateReaded);
+    }
+}
+
+class Booklist {
+    constructor() {
+        this.bookList = [];
+        this.booksRead = [];
+        this.booksNotRead = [];
+        this.nextBookToRead = {};
+        this.currentBook = {};
+        this.lastBookRead = {};
+        
+    }
+
+
+
+    add(book){
+        if (instanceOf(book) === 'Book') throw 'is not a book'
+        this.booklist= [...bookList,book]
+        organize()
+    }
+
+    organize() {
+        this.booksRead = this.list.filter(book => book.isRead === true);
+        this.booksNotRead = this.list.filter(book => book.isRead === false);
+        this.booksRead = this.booksRead.sort((a, b) => b.dateReaded - a.dateReaded)
+    }
+
+    getCurrent(){
+        this.currentBook = this.booksNotRead[0]
+    }
+
+    getNext(){
+        this.nextBookToRead = this.booksNotRead[1]
+    }
+
+    getLast(){
+        this.lastBookRead = this.booksRead[this.booksRead-1]
+    }
+
+    finishCurrentBook () {
+        for (current in bookList){
+            if (this.currentBook === bookList[current]){
+                booklist[current].isRead = true
+                organize()
+                getCurrent()
+                getNext() 
+                getLast() 
+                
+            }
+        }   
+    }
+}
---
+++ ClassTraining/ClassTraining.js
@@ -0,0 +1,41 @@
+class Person {
+    constructor(firtName,lastName,age,gender,interests){
+        this.firtName = firtName
+        this.lastName = lastName
+        this.age = age
+        this.gender = gender
+        this.interests = interests
+        this.fullName = `${this.firtName} ${this.lastName}`
+    }
+    get bio() {
+        return `${this.fullName} is ${this.age} years old. They like ${this.interests}`
+    }
+
+    greeting() {return `Hello, my name is ${this.fullName} `}
+
+}
+
+class Teacher extends Person {
+    constructor(firtName,lastName,age,gender,interests,subject){
+        super(firtName,lastName,age,gender,interests)
+        this.subject = subject
+        this.prefix = this.gender === 'male'? 'Sr.':'Mrs.'
+    }
+
+
+    greeting(){
+        return `Hello, my name is ${this.prefix} ${this.lastName}, and i teach ${this.subject}`
+    }
+
+
+}
+
+class Student extends Person {
+    constructor(firtName,lastName,age,gender,interests){
+        super(firtName,lastName,age,gender,interests)
+    }
+
+    greeting(){
+        return `Yo! I'm ${tgis.firtName}`
+    }
+}
