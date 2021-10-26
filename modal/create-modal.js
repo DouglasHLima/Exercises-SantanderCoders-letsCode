f75d33b98f3a3cdfb958a6f60ff1b7e676682f21
DouglasHLima <douglashdelima@gmail.com>
2021-10-26 19:37:54 -0300

    update to dinamic modal based on input objects

--- modal/create-modal.js
+++ modal/create-modal.js
@@ -1,15 +1,10 @@
 const createModal = (function() {
-    
 
+    const _IS_OPEN_CLASS = '--is-open'
     
     const _modal = document.querySelector('.modal')
     const _modalForm = document.createElement('form')
-    
-    const _inputModalForm = document.createElement('input')
-    _inputModalForm.setAttribute('type','email')
-    _inputModalForm.required = true
-    _inputModalForm.placeholder = 'Digite seu Email'
-    
+
     const _btnCancel = document.createElement('button')
     _btnCancel.setAttribute('type','button')
     _btnCancel.textContent = 'cancel'
@@ -17,19 +12,40 @@ const createModal = (function() {
     const _submitButton = document.createElement('button')
     _submitButton.setAttribute('type','submit')
     _submitButton.textContent = 'ok'
-
-    _modalForm.appendChild(_inputModalForm)
-    _modalForm.appendChild(_submitButton)
-    _modalForm.appendChild(_btnCancel)
-    _modal.appendChild(_modalForm)
     
-    const _IS_OPEN_CLASS = '--is-open'
+    const _createElement = (objOfElement) => {
+        const div = document.createElement('div')
+        const label = document.createElement('label')
+        label.textContent = objOfElement.labelName
+        const input = document.createElement(objOfElement.element)
+        input.setAttribute('type',objOfElement.type)
+        input.setAttribute('name',objOfElement.type)
+        input.placeholder = objOfElement.placeholder
+        input.required = objOfElement.required
+        div.appendChild(label)
+        div.appendChild(input)
 
-    const open = (config) => {
-        _modal.classList.add(_IS_OPEN_CLASS)
-        _setButtons(config.sucess,config.faiokl)
+        return div
     }
+
+    const _makeForm = (settings) => {
+        settings.map(_createElement).forEach( field => {
+            _modalForm.appendChild(field)
+        })
+        _modalForm.appendChild(_submitButton)
+        _modalForm.appendChild(_btnCancel)
+        _modal.appendChild(_modalForm)
+    }
+     
+    const open = (settings,callbackfn) => {
+        _clearForm()
+        _modal.classList.add(_IS_OPEN_CLASS)
+        _makeForm(settings)
+        _setButtons(callbackfn.sucess,callbackfn.fail)
+    }
+
     const _setButtons = (successFn,cancelFn) => {
+        
         _modalForm.onsubmit = (event) => {
             _executeFn(successFn)
             _defaultFormActions()
@@ -40,14 +56,22 @@ const createModal = (function() {
             _defaultFormActions()
         }
     }
+
     const _executeFn = (callbackfn) => {
-        if(callbackfn) callbackfn(_inputModalForm.value)
+        if(callbackfn) callbackfn([..._modalForm.querySelectorAll('input')]
+            .map( input => ({
+                key : input.name,
+                value : input.value
+            }))
+        )
     }
+
+    
+    const _clearForm =() => _modalForm.textContent = ''
 
     const _defaultFormActions = () =>{
         _resetModalForm()
-        _close()
-        
+        _close()  
     }
 
     const _close = () => _modal.classList.remove(_IS_OPEN_CLASS)
--- modal/initializer.js
+++ modal/initializer.js
@@ -1,8 +1,15 @@
 const btnOpenModel = document.querySelector('.btnOpenModal')
 
-const settings = {
-    sucess: email => {console.log('enviando o e-mail: ',email)},
+const settings = [
+    {labelName:'Nome', element: 'input', type:'name', placeholder: 'Insira o primeiro nome', required: true},
+    {labelName:'Sobrenome', element: 'input', type:'lastname', placeholder: 'sobrenome', required: false},
+    {labelName:'Email', element: 'input', type:'email', placeholder:'Insira seu Email', required: true},
+    {labelName:'Resumo', element: 'input', type:'textarea', placeholder:'uma breve descrição', required: false}
+]
+
+const callbackfn = {
+    sucess: value => {console.log('enviando os dados: ',value)},
     fail: () => console.log('cliente cancelou')
 }
 
-btnOpenModel.onclick = function() {createModal.open(settings)}
+btnOpenModel.onclick = () => createModal.open(settings,callbackfn)
